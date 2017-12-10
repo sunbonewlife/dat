3cff1aba73a4cf57bc66f1373e81b51055148d0c
sunbo_newlife <sunbo_newlife@hotmail.com>
2017-12-11 00:56:35 +0800

    20171210

--- dat.py
+++ dat.py
@@ -1,44 +1,47 @@
 import wx
-#from xlwt import *
+import xjxhFrame
+import sys
 
-class MainFrame(wx.Frame):#程序的主界面
+class MainFrame(wx.Frame):
+    # 程序的主界面
     def __init__(self, *args, **kw):
-        # ensure the parent's __init__ is called
         super(MainFrame, self).__init__(*args, **kw)
-        mainBackground = wx.Bitmap("zght.jpg")
-        sb = wx.StaticBitmap(self,bitmap=mainBackground)
-        sb.CentreOnParent()
+        main_background = wx.Bitmap("zght.jpg")
+        sb = wx.StaticBitmap(self, bitmap=main_background)
+        sb.Centre()
         # 创建菜单栏
-        self.makeMenuBar()
+        self.make_menu_bar()
         # 创建状态栏
         self.CreateStatusBar()
         self.SetStatusText("欢迎使用数据分析工具!  如有疑问，请联系八部质量处孙波 手机：15000153349 固话：021-24185194")
         self.SetSize(800,600)
-        self.Centre()
+        self.CentreOnScreen()
+        self.SetBackgroundColour("White")
 
-    def makeMenuBar(self):#定义主界面的菜单
+    def make_menu_bar(self):
+        # 定义主界面的菜单
         # 创建“文件”菜单
-        wenjianMenu = wx.Menu()
-        xinjianxinghaoItem = wenjianMenu.Append(-1, "&新建型号...","创建一个新的型号...")#创建“新建型号...”子菜单，"\t..." 用来定义快捷键
-        tuichuItem = wenjianMenu.Append(-1, "&退出...","退出系统...")
+        file_menu = wx.Menu()
+        new_model_item = file_menu.Append(-1, "&新建型号...","创建一个新的型号...")
+        exit_item = file_menu.Append(-1, "&退出...","退出系统...")
 
-        shujuguanliMenu = wx.Menu()
-        zairushujuItem = shujuguanliMenu.Append(-1,"&载入数据...","载入产品数据表...")
-        chenggongshujuguanliItem = shujuguanliMenu.Append(-1,"&成功数据管理...","管理飞行成功数据样本...")
+        data_manage_menu = wx.Menu()
+        load_data_item = data_manage_menu.Append(-1, "&载入数据...", "载入产品数据表...")
+        success_data_manage_item = data_manage_menu.Append(-1, "&成功数据管理...", "管理飞行成功数据样本...")
 
-        fenxiMenu=wx.Menu()
-        danpiliangfenxiItem=fenxiMenu.Append(-1,"&单批量分析...","绘制某批产品数据的散点图...")
-        duopiliangfenxiItem=fenxiMenu.Append(-1,"&多批量分析...","绘制多批产品数据的散点图...")
+        analysis_menu = wx.Menu()
+        single_batch_item = analysis_menu.Append(-1, "&单批量分析...", "绘制某批产品数据的散点图...")
+        multiple_batch_item = analysis_menu.Append(-1, "&多批量分析...", "绘制多批产品数据的散点图...")
 
-        bangzhuMenu = wx.Menu()
-        caozuobuzhouItem = bangzhuMenu.Append(-1, "&操作步骤...", "获取帮助文件...")
-        guanyuItem = bangzhuMenu.Append(-1, "&关于...", "获取软件信息...")
+        help_menu = wx.Menu()
+        help_step_item = help_menu.Append(-1, "&操作步骤...", "获取帮助文件...")
+        about_item = help_menu.Append(-1, "&关于...", "获取软件信息...")
 
         menuBar = wx.MenuBar()
-        menuBar.Append(wenjianMenu, "&文件")
-        menuBar.Append(shujuguanliMenu, "&数据管理")
-        menuBar.Append(fenxiMenu, "&分析")
-        menuBar.Append(bangzhuMenu, "&帮助")
+        menuBar.Append(file_menu, "&文件")
+        menuBar.Append(data_manage_menu, "&数据管理")
+        menuBar.Append(analysis_menu, "&分析")
+        menuBar.Append(help_menu, "&帮助")
 
         font = menuBar.GetFont()
         font.PointSize += 10
@@ -47,24 +50,25 @@ class MainFrame(wx.Frame):#程序的主界面
         #将菜单条添加到主界面中
         self.SetMenuBar(menuBar)
         #设置每个菜单选项的响应函数
-        self.Bind(wx.EVT_MENU, self.OnXinjianxinghao, xinjianxinghaoItem)
-        self.Bind(wx.EVT_MENU, self.OnTuichu,  tuichuItem)
-        self.Bind(wx.EVT_MENU, self.OnGuanyu, guanyuItem)
+        self.Bind(wx.EVT_MENU, self.on_new_model, new_model_item)
+        self.Bind(wx.EVT_MENU, self.on_exit,  exit_item)
+        self.Bind(wx.EVT_MENU, self.on_about, about_item)
 
-    def OnTuichu(self, event):
-        """Close the frame, terminating the application."""
-        self.Close(True)
+    def on_exit(self, event):
+        sys.exit()
 
-    def OnXinjianxinghao(self, event):
-        self.Close(True)
+    def on_new_model(self, event):
+        XinjianxinghaoFrame = xjxhFrame.NewModelFrame(None, -1, "新建型号...")
+        XinjianxinghaoFrame.CentreOnParent()
+        XinjianxinghaoFrame.ShowModal()
 
-    def OnGuanyu(self, event):
-        wx.MessageBox("Version：0.1.0\n\n编者：八部质量处 孙波"
+    def on_about(self, event):
+        wx.MessageBox("Version：0.1.0.20171210\n\n编者：八部质量处 孙波"
                       "\n电话：021-24185194"
                       "\n         15000153349","关于...")
 
 if __name__ == '__main__':
     app = wx.App()
-    frm = MainFrame(None, title='Data Analysis Tool')
-    frm.Show()
+    wx.MessageBox("欢迎使用数据分析工具！")
+    MainFrame(None, title="Data Analysis Tool  Ver: 0.1.0").Show()
     app.MainLoop()
